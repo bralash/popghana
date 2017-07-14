1a93675dcfcdcbbcb9b75626069a2c5e4826cec0
bra_lash <emmanuelasaber@gmail.com>
2017-07-14 12:52:13 +0000

    debugging

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -19,11 +19,12 @@ class EmailController extends Controller
     }
 
     public function contact(Request $request) {
+    	$userMessage = request()->input('message');
+    	Mail::raw($userMessage, function($message) {
     		$from = request()->input('email-address');
     		$name = request()->input('username');
     		$subject = request()->input('subject');
-    		$userMessage = request()->input('message');
-    	Mail::raw($userMessage, function($message) {
+
      		$message->from($from, $name);
     		$message->subject($subject);
     		$message->to('info@popmoneymatrix.com')->cc('emmanuelasaber@gmail.com');
